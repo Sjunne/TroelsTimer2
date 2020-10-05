542b72e69a6669d3b5ce2e37222ba99730bedba8
Sjunne <sjunneson@hotmail.com>
2020-10-05 06:57:48 +0200

    17/5/2020

---
+++ Assignment1/Data/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using SimpleLogn.Models;
+
+namespace SimpleLogn.Data
+{
+    public interface IUserService
+    {
+        User ValidateUser(string userName, string password);
+    }
+}
---
+++ Assignment1/Forsøg2/Persistence/FileAdapter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Models;
+
+namespace FileData
+{
+    public class FileAdapter : IfileContext
+    {
+        private readonly FileContext FileContext = new FileContext();
+        
+        public void AddAdult(Adult adult)
+        {
+            var fileContextAdults = FileContext.Adults;
+            fileContextAdults.Add(adult);
+            Console.WriteLine(fileContextAdults);
+            FileContext.SaveChanges();
+        }
+
+        public IList<Adult> getAllAdults()
+        {
+            return FileContext.Adults;
+        }
+    }
+}
